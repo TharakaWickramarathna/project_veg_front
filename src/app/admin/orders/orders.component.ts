ed508fb8f53df51f65cd7221a7dfaae10c7f62e1
udithaabeykoon <udithaedu@gmail.com>
2020-05-31 02:01:05 +0530

    admin order

--- src/app/admin/orders/orders.component.ts
+++ src/app/admin/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from 'src/app/shared/services/orders-service.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ordersService:OrdersService) { }
 
   ngOnInit(): void {
+    this.ordersService.fetchAllOrdersFromDatabase().subscribe((res)=>{
+      console.log(res);
+    });
   }
+
+
 
 }
--- src/app/shared/services/orders-service.service.ts
+++ src/app/shared/services/orders-service.service.ts
@@ -11,6 +11,7 @@ export class OrdersService {
 
   private CONFIRM_ORDER_URL = "http://localhost:5000/orders/add";
   private GET_ORDERS_URL = "http://localhost:5000/orders/";
+  private GET_ALL_ORDERS_URL = "http://localhost:5000/orders/all";
 
   confirmOrder(orderObject:{clientID:string,products:{_id:string,quantity:number,isPack:string}[]}){
     return this.http.post(this.CONFIRM_ORDER_URL,orderObject);
@@ -18,5 +19,9 @@ export class OrdersService {
 
   fetchOrdersFromDatabase(clientID:string){
     return this.http.get<IncomingOrdersModel[]>(this.GET_ORDERS_URL+clientID);
+  }
+
+  fetchAllOrdersFromDatabase(){
+    return this.http.get<IncomingOrdersModel[]>(this.GET_ALL_ORDERS_URL);
   }
 }
