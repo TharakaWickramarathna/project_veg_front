b8f012f579813c8636c041062de9b42d10054f4c
udithaabeykoon <udithaedu@gmail.com>
2020-06-05 20:45:10 +0530

    aa

--- src/app/my-orders/completed-orders/completed-orders.component.ts
+++ src/app/my-orders/completed-orders/completed-orders.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CompletedOrdersComponent implements OnInit {
 
   headElements = ['Order ID', 'Date' ,'Time', 'Address', 'Total','Order State'];
+  completedOrders=[];
 
   constructor() { }
 
